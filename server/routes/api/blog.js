8a4e8e602847e04d31f4bab115343016d3b37130
Kiran_Kulkarni_CronJ <kiran.kulkarni@cronj.com>
2019-01-30 23:04:45 +0530

    added all

--- server/routes/api/blog.js
+++ server/routes/api/blog.js
@@ -6,13 +6,17 @@ var keystone = require('keystone');
 
 // Getting our page model
 var Blog = keystone.list('Blog');
+var BlogGet = keystone.get('Blog');
+
 
 // Creating the API end point
 // More about keystone api here: https://gist.github.com/JedWatson/9741171
 exports.list = function (req, res) {
 	// Querying the data this works similarly to the Mongo db.collection.find() method
-	Blog.model.find(function (err, items) {
-		// Make sure we are handling errors
+	Blog.paginate({
+		page: req.query.page || 1,
+		perPage: 3,
+	}).exec(function (err, items) {
 		if (err) return res.apiError('database error', err);
 		res.apiResponse({
 			// Filter page by
@@ -20,6 +24,18 @@ exports.list = function (req, res) {
 		});
 
 	// Using express req.query we can limit the number of recipes returned by setting a limit property in the link
-	// This is handy if we want to speed up loading times once our recipe collection grows
-	}).limit(Number(req.query.limit));
+	// This is handy if we want to speed up loading times once our blog collection grows
+	});
+};
+
+
+exports.get = function (req, res) {
+	console.log('---->', req.params.id);
+	BlogGet.model.findOne().where('id', req.params.id).exec(function (err, item) {
+		if (err) return res.apiError('database error', err);
+		if (!item) return res.apiError('not found');
+		res.apiResponse({
+			blog: item,
+		});
+	});
 };
--- server/routes/api/lecture.js
+++ server/routes/api/lecture.js
@@ -11,15 +11,16 @@ var Lecture = keystone.list('Lecture');
 // More about keystone api here: https://gist.github.com/JedWatson/9741171
 exports.list = function (req, res) {
 	// Querying the data this works similarly to the Mongo db.collection.find() method
-	Lecture.model.find(function (err, items) {
-		// Make sure we are handling errors
+	Lecture.paginate({
+		page: req.query.page || 1,
+		perPage: 3,
+	}).exec(function (err, items) {
 		if (err) return res.apiError('database error', err);
 		res.apiResponse({
 			// Filter page by
 			lecture: items,
 		});
-
 	// Using express req.query we can limit the number of recipes returned by setting a limit property in the link
 	// This is handy if we want to speed up loading times once our recipe collection grows
-	}).limit(Number(req.query.limit));
+	});
 };
