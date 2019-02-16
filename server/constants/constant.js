42de2657dc1922a482ca414fadd27d120821a1c0
Kiran_Kulkarni_CronJ <kiran.kulkarni@cronj.com>
2019-02-16 17:18:07 +0530

    EMAIL INTEGRATION- SENDGRID, NODEMAILER etc

--- server/constants/constant.js
+++ server/constants/constant.js
@@ -6,7 +6,24 @@ exports.CONSTANTS = {
         FROM_NAME: 'H.H. Niranjana Swami',
         FROM_EMAIL: 'kiran.kulkarni@cronj.com',
         SUBJECT : 'Appointment approved by H.H. Niranjana Swami',
+        SENDGRID_API_KEY: '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',
+        SENDGRID_HTML:'<strong>You can visit</strong>',
+        NODE_MAILER : {
+            mail: {
+              smtpConfig: {
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true, // use SSL 
+                auth: {
+          
+                  user: 'hhnsweb@gmail.com',
+                  pass: 'hhns_2019'
+                }
+              },
+              sender: '"HHNS" <hhnsweb@gmail.com>' // sender address 
+            }
     }
 
 
+}
 }
