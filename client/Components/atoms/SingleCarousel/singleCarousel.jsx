ea17c5efff8306ce2bdbee3b0ee07bd3b1d87d06
Shailendra12219 <shailendra@cronj.com>
2019-03-01 19:28:39 +0530

    Changes in UI

--- client/Components/atoms/SingleCarousel/singleCarousel.jsx
+++ client/Components/atoms/SingleCarousel/singleCarousel.jsx
@@ -6,7 +6,7 @@ const singleCarousel = (props) => {
         <div className="slide-inner">
             <div className="container textCarousel">
                 <div className="swiper-slide-caption">
-                    <h1 className="wow-outer"><span className="font-weight-light wow-outer"><span className="wow" data-caption-animate="slideInDown">{props.heading} </span></span><span className="font-weight-bold wow-outer"><span className="wow" data-caption-animate="slideInDown" data-wow-delay="0">{props.text}</span></span></h1>
+                    <h1 className="wow-outer carouselFont"><span className="font-weight-light wow-outer"><span className="wow" data-caption-animate="slideInDown">{props.heading} </span></span><span className="font-weight-bold wow-outer"><span className="wow" data-caption-animate="slideInDown" data-wow-delay="0">{props.text}</span></span></h1>
                 </div>
             </div>
         </div>
--- client/Components/molocules/carousels/Carousel.jsx
+++ client/Components/molocules/carousels/Carousel.jsx
@@ -28,12 +28,20 @@ export class Carousel extends Component {
                             </Translate>} />
                         <SingleCarousel
                             image="https://ik.imagekit.io/gcwjdmqwwznjl/Home_Page-carousel_picture-abhishek_no_logo_rk3rB-HLV.jpg"
-                            heading=""
-                            text="" />
+                            heading={<Translate>
+                                {({ translate }) => <h1>{translate("HOME.official_website")}</h1>}
+                            </Translate>}
+                            text={<Translate>
+                                {({ translate }) => <h1>{translate("HOME.h_h")}</h1>}
+                                </Translate>} />
                         <SingleCarousel
                             image="https://ik.imagekit.io/gcwjdmqwwznjl/Home_Page-carousel_picture_kirtan_no_logo_HJiHHWrLN.jpg"
-                            heading=""
-                            text="" />
+                            heading={<Translate>
+                                {({ translate }) => <h1>{translate("HOME.official_website")}</h1>}
+                            </Translate>}
+                            text={<Translate>
+                                {({ translate }) => <h1>{translate("HOME.h_h")}</h1>}
+                                </Translate>} />
 
                     </div>
                     <div className="swiper-pagination-outer container alignment">
