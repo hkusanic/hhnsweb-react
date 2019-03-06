8ad643c0c7f4eea431c2f9cfad4ae46d353cfe84
Kiran_Kulkarni_CronJ <kiran.kulkarni@cronj.com>
2019-03-06 21:05:46 +0530

    appointment-listing-apis

--- server/routes/api/appointment.js
+++ server/routes/api/appointment.js
@@ -4,6 +4,21 @@ var Appointment = keystone.list('Appointment');
 
 
 
+exports.list = function (req, res) {
+	// Querying the data this works similarly to the Mongo db.collection.find() method
+	Appointment.model.find(function (err, items) {
+		// Make sure we are handling errors
+		if (err) return res.apiError('database error', err);
+		res.apiResponse({
+			// Filter page by
+			appointments: items,
+		});
+
+	// Using express req.query we can limit the number of recipes returned by setting a limit property in the link
+	// This is handy if we want to speed up loading times once our recipe collection grows
+	}).limit(Number(req.query.limit));
+};
+
 exports.create = function(req, res) {
 	
 	var item = new Appointment.model(),
