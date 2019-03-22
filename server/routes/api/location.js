e8f085790b8bd026f3033521509923b9c1274341
Kiran_Kulkarni_CronJ <kiran.kulkarni@cronj.com>
2019-03-22 18:50:33 +0530

    apis for create location and lecture

--- server/routes/api/location.js
+++ server/routes/api/location.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+let logger = require('./../../logger/logger');
 
 
 var Location = keystone.list('Location');
@@ -14,3 +15,46 @@ exports.list = function (req, res) {
 		});
 	}).sort('title');
 };
+
+
+exports.createBulk = function (req, res) {
+    logger.info({
+		req: req
+	}, "API createBulk location");
+	keystone.createItems({
+	      Location: req.body
+	}, function (err, stats) {
+		if (err) {
+			logger.error({
+				error: err
+			}, "API createBulk location");
+			return res.apiError('error', err);
+		}
+		return res.apiResponse({
+			Location: true
+		});
+	});
+}
+
+exports.create = function (req, res) {
+    
+	var item = new Location.model(),
+		data = (req.method == 'POST') ? req.body : req.query;
+		logger.info({
+			req: req
+		}, "API create location");
+	item.getUpdateHandler(req).process(data, function (err) {
+
+		if (err) {
+			logger.error({
+				error: err
+			}, "API create location");
+			return res.apiError('error', err);
+		}
+
+		res.apiResponse({
+			Location: item
+		});
+
+	});
+}
