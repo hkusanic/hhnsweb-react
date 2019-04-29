ce1dbddb0f6c9b7d2aee8d76b7bc43325a50c39a
Kiran_Kulkarni_CronJ <kiran.kulkarni@cronj.com>
2019-04-29 11:52:23 +0530

    content apis

--- server/models/Lecture.js
+++ server/models/Lecture.js
@@ -1,5 +1,6 @@
 const keystone = require('keystone');
 const Types = keystone.Field.Types;
+var Content = keystone.list('Content');
 
 let Lecture = new keystone.List('Lecture', {
 	autokey: { path: 'slug', from: 'uuid', unique: true },
@@ -79,8 +80,51 @@ Lecture.schema.pre('save', function (next) {
 	next();
 });
 
-Lecture.schema.post('save', function (next) {
-	// next();
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function todayDate () {
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth() + 1; // January is 0!
+
+	var yyyy = today.getFullYear();
+	if (dd < 10) {
+		dd = '0' + dd;
+	}
+	if (mm < 10) {
+		mm = '0' + mm;
+	}
+	var today = yyyy + '-' + mm + '-' + dd;
+	return today;
+}
+
+Lecture.schema.post('save', function (data, next) {
+
+   
+    var item = new Content.model();
+    let body = {};
+    body.content_uuid = data.uuid;
+    body.uuid = uuidv4();
+    body.content_type = 'Lecture';
+    body.date_created = todayDate();
+    
+	item.getUpdateHandler().process(body, function (err) {
+
+		if (err) {
+			logger.error({
+				error: err,
+			}, 'API create content');
+		
+		}
+
+    });
+    
+    next();
 });
 
 Lecture.schema.post('validate', function (err, next) {
