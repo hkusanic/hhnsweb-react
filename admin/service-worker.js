933aa83d2e0ea2674b25ff0812c4bdc6ae13f75d
Shailendra12219 <shailendra@cronj.com>
2019-05-20 21:50:59 +0530

    admin build added

--- admin/service-worker.js
+++ admin/service-worker.js
@@ -14,7 +14,7 @@
 // importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 // importScripts(
-//   "/precache-manifest.87c9fe2b39af20c8b1cc6836d8a6a368.js"
+//   "/precache-manifest.2d837728156d668af3a8b057e7e8eb47.js"
 // );
 
 // workbox.clientsClaim();
