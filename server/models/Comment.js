e733c3ad6dcf5ca55ebf9bf27a5634a96cc50ff7
roshni <roshni@cronj.com>
2019-05-29 14:12:54 +0530

    added comments list and other fixes

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,21 +1,27 @@
-const keystone = require('keystone');
+const keystone = require("keystone");
 const Types = keystone.Field.Types;
 
-let Comment = new keystone.List('Comment', {
-	autokey: { path: 'slug', from: 'dateCreated _id', unique: true },
-	map: { name: 'message' },
-	defaultSort: '-dateCreated',
+let Comment = new keystone.List("Comment", {
+	autokey: { path: "slug", from: "dateCreated _id", unique: true },
+	map: { name: "message" },
+	defaultSort: "-created_date_time"
 });
 
 Comment.add({
 	uuid: { type: String, unique: true, index: true },
-	message: { type: String, initial: true, required: true, index: true, default: '' },
+	message: {
+		type: String,
+		initial: true,
+		required: true,
+		index: true,
+		default: ""
+	},
 	author_name: { type: String },
 	author_email: { type: String },
 	lecture_uuid: { type: String },
 	approved: { type: Boolean },
 	dateCreated: { type: Types.Date, default: Date.now },
+	created_date_time: { type: Types.Date, default: Date.now }
 });
-
 
 Comment.register();
