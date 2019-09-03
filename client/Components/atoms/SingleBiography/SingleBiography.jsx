8642533a486b2ef3aee2675e89a4c87e99377f9b
Shailendra12219 <priyanka@cronj.com>
2019-09-03 20:36:00 +0530

    homepage changes

--- client/Components/atoms/SingleBiography/SingleBiography.jsx
+++ client/Components/atoms/SingleBiography/SingleBiography.jsx
@@ -58,71 +58,58 @@ const SingleBiography = props => {
 							/>
 						}
 					>
-<<<<<<< HEAD
 						<div
-							style={{
-								paddingTop: "24px !important",
-								paddingLeft: "0px !important",
-								paddingRight: "0px !important"
+							className={
+								props.title_en === "His Holiness Niranjana Swami"
+									? "singleBiography_card_orange"
+									: "singleBiography_card_white"
+							}
+						>
+							{reactCookie.load("languageCode") === "en"
+								? props.title_en
+								: props.title_ru
+								? props.title_ru
+								: props.title_en}
+						</div>
+						<br />
+						<div
+							className={
+								props.title_en === "His Holiness Niranjana Swami"
+									? "singleBiography_text_orange"
+									: "singleBiography_text_white"
+							}
+						>
+							{renderHTML(
+								"<div > " +
+									showing100Characters(
+										reactCookie.load("languageCode") === "en"
+											? props.content_en
+											: props.content_ru
+											? props.content_ru
+											: props.content_en
+									) +
+									"</div>"
+							)}
+							&nbsp;
+						</div>
+						<Link
+							to={{
+								pathname: "/biograhyDetails",
+								state: {
+									title_en: props.title_en,
+									title_ru: props.title_ru,
+									content_en: props.content_en,
+									content_ru: props.content_ru,
+									img: props.img
+								}
 							}}
 						>
-							<div
-								className={
-									props.title_en === "His Holiness Niranjana Swami"
-										? "singleBiography_card_orange"
-										: "singleBiography_card_white"
-								}
-							>
-=======
-						<div>
-							<div class="singleBiography_card">
->>>>>>> f7e15012e774ef34dbafbf5158cca853370b6ce6
-								{reactCookie.load("languageCode") === "en"
-									? props.title_en
-									: props.title_ru
-									? props.title_ru
-									: props.title_en}
-							</div>
-							<br />
-							<div
-								className={
-									props.title_en === "His Holiness Niranjana Swami"
-										? "singleBiography_text_orange"
-										: "singleBiography_text_white"
-								}
-							>
-								{renderHTML(
-									"<div > " +
-										showing100Characters(
-											reactCookie.load("languageCode") === "en"
-												? props.content_en
-												: props.content_ru
-												? props.content_ru
-												: props.content_en
-										) +
-										"</div>"
-								)}
-								&nbsp;
-							</div>
-							<Link
-								to={{
-									pathname: "/biograhyDetails",
-									state: {
-										title_en: props.title_en,
-										title_ru: props.title_ru,
-										content_en: props.content_en,
-										content_ru: props.content_ru,
-										img: props.img
-									}
-								}}
-							>
-								<button className="singleBiography_button">
-									<Translate>
-										{({ translate }) => translate("FOOTER.readmore")}
-									</Translate>
-								</button>
-							</Link>
-						</div>
+							<button className="singleBiography_button">
+								<Translate>
+									{({ translate }) => translate("FOOTER.readmore")}
+								</Translate>
+							</button>
+						</Link>
 					</Card>
 				</Link>
 			</article>
